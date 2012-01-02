f515f4cc343c684a33a04ef529940e9d3df1e8f3
zoibot <kevin@zoibot.com>
2012-01-01 17:57:59 -0800

    Fixed webgl bugs

--- graphics.js
+++ graphics.js
@@ -1,14 +1,18 @@
-var gl = null;
 
 graphics = function() {
     g = {};
     g.pMatrix = mat4.create();
     g.mvMatrix = null;
+    var gl = null;
     g.init = function () {
         var cv = $("<canvas>", {"width": 300, "height": 300});
         $(document).append(cv);
         gl = cv[0].getContext("experimental-webgl");
+        if(gl === null) {
+            gl = cv[0].getContext("webgl");
+        }
         this.pMatrix = mat4.ortho(0, 300, 300, 0, -1, 1);
+        console.log('graphics initialized '+gl);
     };
     g.draw = function (objects) {
     };
