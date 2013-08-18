1b72a29c04e267575e3492f4906eca0d5bcdb3c2
zoibot <kevin@zoibot.com>
2013-08-17 22:42:45 -0700

    support equipping and using items on other items

--- js/objects/item.js
+++ js/objects/item.js
@@ -11,19 +11,34 @@
     };
     item.prototype.draw = function (ctx) {
         this.graphic.draw(ctx, this.x, this.y, this.angle, 'default');
-    },
+    };
     item.prototype.update = function () {
-    },
+    };
     item.prototype.hits = function (ex, ey) {
         return Math.abs(ex - this.x) + Math.abs(ey - this.y) < 20;
-    },
+    };
     item.prototype.handle = function (ev) {
         if (ev.type === "hit" && this.description) {
-            // get item
-            action_queue.enqueue(new actions.textbox(this.description));
-            world.remove_object(this);
-            inventory.add_item(this.item_details);
+            if (this.item_details.grabbable)
+            {
+                // get item
+                action_queue.enqueue(new actions.textbox(this.description));
+                world.remove_object(this);
+                inventory.add_item(this.item_details);
+            }
         }
-    }
+        else if (this.item_details.handlers && ev.type in this.item_details.handlers)
+        {
+            // TODO how to encode these better
+            action_queue.enqueue(new actions.textbox(this.item_details.handlers[ev.type]));
+        }
+    };
+    item.prototype.activate = function (x, y, player) {
+        var ev = {};
+        ev.type = this.item_details.activate[0];
+        ev.x = x;
+        ev.y = y;
+        return ev;
+    };
 })();
 
